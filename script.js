e91494303101596bc75db57513e42af6c9fb872a
jigerjoock4-code <jigerjoock4@gmail.com>
2026-04-23 16:32:06 +0200

    a

---
+++ script.js
@@ -0,0 +1,21 @@
+let alter = 13
+const alterVonName = 14
+
+let hausnummer = 18
+let lichtIstAn = true
+let contacts = ["arda" ," Trump","elon Musk",4,true,[4,5]]
+let profil = {
+name:"finn",
+tel:21772283,
+adresse:"heilbronn str weinsbergerstrase nr 5",
+gleicheFreunde:{
+fr1:"ben",
+fr2:"simon",
+
+}
+}
+
+
+console.log()
+
+
