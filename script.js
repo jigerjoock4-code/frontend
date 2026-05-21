d00ebe0780f97b9f73b4df4c265a7a3d9a9fd5d5
jigerjoock4-code <jigerjoock4@gmail.com>
2026-05-21 18:18:07 +0200

    sa

--- script.js
+++ script.js
@@ -33,14 +33,24 @@ let div = document.querySelector("#chat-area")
 
 
     for (let i = 0; i<nachrichten.length; i++ ){
-    let nachrichtenBlase = document.createElement("div")
+
+
+    let nachrichtenBlase  = document.createElement("div")
+
+    if (nachrichten[i].sender=="Du") {
+    nachrichtenBlase.classList.add("meineNachrichten")
+    }
+
     nachrichtenBlase.classList.add("msg")
 
-    let sender = document.createElement("h3")
-    sender.textContent = nachrichten[i].sender
+  
+
+let sender = document.createElement("h1")
+let msg = document.createElement("p")
+
+sender.textContent=nachrichten[i].sender
+msg.textContent=nachrichten[i].text
     nachrichtenBlase.appendChild(sender)
-    let msg = document.createElement("p")
-    msg.textContent = nachrichten[i].text
     nachrichtenBlase.appendChild(msg)
 
 
@@ -48,6 +58,7 @@ let div = document.querySelector("#chat-area")
 
 
     div.appendChild(nachrichtenBlase)
+    
     
 }
 
@@ -76,13 +87,6 @@ for(let i = 0; i<kontakte.length; i++){
     let div = document.createElement("div")
     div.textContent=kontakte[i]
     kontakteDiv.appendChild(div)
-
-
-
-
-
 }
 
 
-
- 
